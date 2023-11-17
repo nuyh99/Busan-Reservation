256d1773e784dc8d04c7705beb8a08c3086aae37
nuyh <hjaehyun25@gmail.com>
2023-11-18 00:48:19 +0900

    feat: cors 전체 허용

--- src/main/java/com/example/busan/common/config/WebConfigurer.java
+++ src/main/java/com/example/busan/common/config/WebConfigurer.java
@@ -3,6 +3,7 @@ package com.example.busan.common.config;
 import com.example.busan.auth.AuthorizationArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -19,5 +20,11 @@ public class WebConfigurer implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(resolver);
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*");
     }
 }
