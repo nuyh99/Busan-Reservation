5049b12989bbd76bd53545a8490e533680ae4d87
nuyh <hjaehyun25@gmail.com>
2023-11-27 16:31:41 +0900

    feat: cors 오리진 추가

--- src/main/java/com/example/busan/common/config/WebConfigurer.java
+++ src/main/java/com/example/busan/common/config/WebConfigurer.java
@@ -25,7 +25,7 @@ public class WebConfigurer implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://172.17.0.1:8080")
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
